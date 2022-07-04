24a4a14a4d95d74e38ab797f2e06ee0f94ea2aba
Igor Lech <89975061+igorlech@users.noreply.github.com>
2022-07-04 11:14:41 +0200

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,12 +1,13 @@
 let i = 0;
-let txt = 'his page is under construction. If you are looking for something, please visit my GitHub profile:';
+let txt = 'This page is under construction. If you are looking for something, please visit my GitHub profile:';
 let speed = 40;
 
 function typeWriter() {
   if (i < txt.length) {
-    document.getElementById("typetext").innerHTML += txt.charAt(i);
+    document.querySelector("#typetext").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
 }
+
 typeWriter()
---
+++ js/javascript2.js
@@ -0,0 +1,39 @@
+var typeText = document.querySelector(".typeText")
+var textToBeTyped = "This page is under construction. If you are looking for something, please visit my GitHub profile:"
+var index = 0, isAdding = true
+
+function playAnim() {
+  setTimeout(function () {
+    // set the text of typeText to a substring of
+    // the textToBeTyped using index.
+    typeText.innerText = textToBeTyped.slice(0, index)
+    if (isAdding) {
+      // adding text
+      if (index > textToBeTyped.length) {
+        // no more text to add
+        isAdding = false
+        //break: wait 2s before playing again
+        setTimeout( function () {
+          playAnim()
+        }, 1000)
+        return
+      } else {
+        // increment index by 1
+        index++
+      }
+    } else {
+      // removing text
+      if (index === 0) {
+        // no more text to remove
+        isAdding = true
+      } else {
+        // decrement index by 1
+        index--
+      }
+    }
+    // call itself
+    playAnim()
+  }, 120)
+}
+// start animation
+playAnim()
